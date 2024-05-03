05eb7f9e21dc032ea62c539298438d0512291fd5
Sahar Levy <sahar@nogalesfundmgmt.com>
2024-05-03 15:04:55 -0700

    done

--- frontend/components/DogForm.js
+++ frontend/components/DogForm.js
@@ -1,12 +1,64 @@
-import React, { useState } from 'react'
+import React, { useState, useEffect } from 'react'
+import { useNavigate } from 'react-router-dom'
 
 const initialForm = { name: '', breed: '', adopted: false }
 
 // Use this form for both POST and PUT requests!
-export default function DogForm() {
+export default function DogForm({ dog, reset, getDogs }) {
+  const navigate = useNavigate()
   const [values, setValues] = useState(initialForm)
+  const [breeds, setBreeds] = useState([])
+
+  useEffect(() =>{
+    fetch('/api/dogs/breeds')
+      .then(res => res.json())
+      .then(breeds => setBreeds(breeds.toSorted())) //toSorted returns a new array, does not mutate original
+      .catch(err => console.error(err))
+  }, [])
+
+  // effect that fires on change of dog
+  useEffect(() => {
+    if (dog) setValues(dog) //this makes it so that when 'edit' mode, dog's details populate in form to be edited
+    else setValues(initialForm) //this resets form once edit mode is exited
+  }, [dog])
+
+  const postDog = () => {
+    fetch('/api/dogs', {
+      method: 'POST',
+      body: JSON.stringify(values),
+      headers: new Headers({'Content-Type': 'application/json'})
+    })
+      .then(res => {
+        if (!res.ok) throw new Error('Problem POSTing dog')
+        getDogs()
+        navigate('/') //this also takes care of clearing values in the form bc the whole form will become unmounted
+      })
+      .catch(err => console.error(err))
+  }
+  const putDog = () => {
+    fetch(`/api/dogs/${values.id}`, { //the id of the dog is curently being held inside the state of the form, dog is set inside the state of the form.
+      method: 'PUT',
+      body: JSON.stringify(values),
+      headers: new Headers({'Content-Type': 'application/json'})
+    })
+      .then(res => {
+        if (!res.ok) throw new Error('Problem PUTing dog')
+        getDogs()
+        reset()
+        navigate('/') //this also takes care of clearing values in the form bc the whole form will become unmounted
+      })
+      .catch(err => console.error(err))
+  }
+  const onReset = (e) => {
+    e.preventDefault()
+    setValues(initialForm)
+    reset()
+  }
+  
   const onSubmit = (event) => {
     event.preventDefault()
+    const action = dog ? putDog : postDog
+    action()
   }
   const onChange = (event) => {
     const { name, value, type, checked } = event.target
@@ -17,7 +69,7 @@ export default function DogForm() {
   return (
     <div>
       <h2>
-        Create Dog
+      {dog ? 'Update Dog' : 'Create Dog'} 
       </h2>
       <form onSubmit={onSubmit}>
         <input
@@ -35,6 +87,7 @@ export default function DogForm() {
         >
           <option value="">---Select Breed---</option>
           {/* Populate this dropdown using data obtained from the API */}
+          {breeds.map(breed => <option key={breed}>{breed}</option>)}
         </select>
         <label>
           Adopted: <input
@@ -47,9 +100,9 @@ export default function DogForm() {
         </label>
         <div>
           <button type="submit">
-            Create Dog
+            {dog ? 'Update Dog' : 'Create Dog'} 
           </button>
-          <button aria-label="Reset form">Reset</button>
+          <button onClick={onReset} aria-label="Reset form">Reset</button>
         </div>
       </form>
     </div>
--- frontend/components/DogsList.js
+++ frontend/components/DogsList.js
@@ -1,17 +1,40 @@
 import React from 'react'
+import { useNavigate } from 'react-router-dom'
 
-export default function DogsList() {
+export default function DogsList({ dogs, getDogs, setCurrentDog }) {
+  const navigate = useNavigate()
+
+  const editDog = id => {
+    // set the id as the current id
+    setCurrentDog(id) 
+    // navigate to the form
+    navigate('form')
+  }
+  const deleteDog = id => {
+    fetch(`/api/dogs/${id}`, {method: 'DELETE'})
+      .then(res => {
+        if (!res.ok) throw new Error('Problem DELETing dog')
+        getDogs()
+        setCurrentDog(null) // clears the edit form
+    })
+    .catch(err => console.error(err))
+  }
+
   return (
     <div>
       <h2>Dogs Shelter</h2>
       <ul>
-        <li>
-          Fido, Bulldog, NOT adopted
-          <div>
-            <button>Edit</button>
-            <button>Delete</button>
-          </div>
-        </li>
+        {
+          dogs.map(dog => (
+            <li key={dog.id}>
+              {dog.name}, {dog.breed}, {dog.adopted ? '' : 'NOT'}adopted
+              <div>
+                <button onClick={() => editDog(dog.id)}>Edit</button>
+                <button onClick={() => deleteDog(dog.id)}>Delete</button>
+              </div>
+            </li>
+          ))
+        }
       </ul>
     </div>
   )
